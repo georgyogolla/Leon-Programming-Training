7d8a5d4d46f737b4b2397a27f2d51cb92757a228
georgyogolla <georgyogolla@gmail.com>
2026-02-18 14:01:00 +0300

    Numbers in Javascript

---
+++ Javascript/numbers.js
@@ -0,0 +1,27 @@
+
+// JavaScript has ONE Number type
+// Positive numbers!
+// Negative numbers!
+// Whole number (integers)!
+// Decimal numbers!
+
+// SIMPLE OPERATIONS
+// Addition 
+console.log(1+1);
+// Subtraction 
+console.log(100 - 20);
+// Division
+console.log(20/10);
+// Multiplication
+console.log(40 * 5);
+// Modulo
+console.log(39 % 5);
+
+// NaN
+// NOT A NUMBER
+// NaN is a numeric value that represents
+// something that is not...a number
+console.log(0/0);
+console.log(1+NaN);
+
+
---
+++ Javascript/test.js
@@ -0,0 +1,4 @@
+// console.log("Hello World!"); String
+// console.log(1 + 1); Number
+// console.log(true);Boolean
+console.log(null);
