ad9fda2386380c9633b87eff868848bd9b3a0210
georgyogolla <georgyogolla@gmail.com>
2026-02-22 14:06:07 +0300

    Introduction to variables

---
+++ Javascript/variables/index.js
@@ -0,0 +1,22 @@
+// Variables are like "labeled jars"
+// for a value in JavaScript.
+// We can store a value and give it a
+// name, so that we can...
+// - recall it
+// - use it
+// - or change it later on.
+
+// BASIC SYNTAX
+
+// **recall it**
+// let someName = value;
+// let name = "Leon";
+// console.log(name); 
+
+// **change it later on**
+let name;
+name = "Kabambe";
+name = "Joshua"; 
+console.log(name);
+
+
