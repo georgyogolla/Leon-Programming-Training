3799974903e27398097adecebbe5fae7d3b114ae
georgyogolla <georgyogolla@gmail.com>
2026-04-09 15:02:40 +0300

    Introduction to forLoops

---
+++ Javascript/loops/forLoops.js
@@ -0,0 +1,39 @@
+// LOOPS
+// Doing things repeatedly
+// Loops allow us to repeat code
+// "Print 'hello' 10 times
+// Sum all numbers in an array
+// There are multiple types:
+// for loop
+// while loop
+// for...of loop
+// for...in loop
+
+
+// SYNTAX 
+
+// for (
+// [initialExpression];
+// [condition];
+// [incrementExpression]
+// )
+// i = 0;      // initial expression
+// if i < 10; // condition
+// i =+ 1    // increment expression
+
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+};
+
+// Example 2
+
+// Start i at 50
+// Subtract 10 each iteration
+// Keep going as long as i >= 0
+
+for(let i = 50; i >= 0; i -= 10){
+    console.log(i);
+}
+
+// i += 1 => i = i + 1
+// i -=10 => i = i - 10
